1f34e998176eb85381bba838cdc4204f222f50eb
wailorman <wailorman@gmail.com>
2016-01-27 23:39:53 +0300

    refactor: To ES6

--- public/src/page_script.js
+++ public/src/page_script.js
@@ -1,13 +1,13 @@
-var isInvitingProceed = false;
-var invitedCounter = 0;
+let isInvitingProceed = false;
+let invitedCounter = 0;
 
-var userContainerIndex = 0;
-var invitingInterval = null;
+let userContainerIndex = 0;
+let invitingInterval = null;
 
 const toggleInvitingSelector = '#sk_auto';
 
 
-setInterval(function () {
+setInterval(() => {
 
     const controlPanelSelector = '#inviterControlPanel';
 
@@ -16,14 +16,14 @@ setInterval(function () {
         // inject control panel
         $('#hook_Form_OnSiteNowUsersRBFormForm').append('<div id="inviterControlPanel"><center><button id="gotoBabyli">Перейти к бабулям ----(^.^)</button></center><br><center><button id="sk_auto">НАЧАТЬ UHBAUTUNG!</button><br><br><span>Пригласили: <span id="invitedDiv">0</span></span></center></div>');
 
-        $('#gotoBabyli').click(function () {
+        $('#gotoBabyli').click(() => {
             // set filter to target audience (grannies)
             $('#field_ageTo').val(90).change();
             $('#field_ageFrom').val(50).change();
             $('#field_male').attr('checked', false).change();
         });
 
-        $(toggleInvitingSelector).click(function () {
+        $(toggleInvitingSelector).click(() => {
             if (isInvitingProceed == false) {
                 startInviting();
             } else {
@@ -31,10 +31,10 @@ setInterval(function () {
             }
         });
 
-        var controlPanelWidth = $(controlPanelSelector).width();
+        let controlPanelWidth = $(controlPanelSelector).width();
         $(controlPanelSelector).css({background: 'rgb(240, 240, 240)', padding: '20px 0px', width: controlPanelWidth});
-        $(window).scroll(function (e) {
-            var viewPortTopPointPositionOnDocument = $(window).scrollTop();
+        $(window).scroll(()=>{
+            let viewPortTopPointPositionOnDocument = $(window).scrollTop();
             if (viewPortTopPointPositionOnDocument > 310) {
                 // keep control panel visible when scroll
                 $(controlPanelSelector).css({position: 'fixed', top: 110});
@@ -59,16 +59,16 @@ function startInviting() {
     isInvitingProceed = true;
     $(toggleInvitingSelector).text("ОСТАНОВИТЬ UHBAUTUNG!");
 
-    invitingInterval = setInterval(function () {
+    invitingInterval = setInterval(() => {
 
         // photoWrapper is div class name of the user avatar in search list
 
-        var userContainer = $($(userContainerSelector)[userContainerIndex]);
-        var userId;
+        let userContainer = $($(userContainerSelector)[userContainerIndex]);
+        let userId;
         if (userContainer.find('img').length > 0 || userContainer.find('.onbigavcont').length > 0) {
 
-            var getUserAvatarByWrapper = function (photoWrapper) {
-                var userAvatar = photoWrapper.find('img');
+            let getUserAvatarByWrapper = function (photoWrapper) {
+                let userAvatar = photoWrapper.find('img');
                 if (userAvatar.length == 0) {
                     userAvatar = photoWrapper.find(".onbigavcont");
                 }
@@ -77,7 +77,7 @@ function startInviting() {
             };
 
 
-            var userAvatar = getUserAvatarByWrapper(userContainer);
+            let userAvatar = getUserAvatarByWrapper(userContainer);
 
             userId = userContainer.find('a').attr('href').split('/')[2];
             $(window).scrollTop(userContainer.offset().top - 150); // scroll to inviting target
@@ -91,13 +91,13 @@ function startInviting() {
                     'button_invite': 'clickOverGWT'
                 },
                 type: "POST",
-                beforeSend: function (xhr) {
+                beforeSend: xhr => {
                     xhr.setRequestHeader('TKN', OK.tkn.get());
                 }
-            }).success(function (data) {
+            }).success(data => {
 
-                var tooOften = data.indexOf('слишком часто') > -1;
-                var userNotReceiveInvites = data.indexOf('не принимает приглашения') > -1;
+                let tooOften = data.indexOf('слишком часто') > -1;
+                let userNotReceiveInvites = data.indexOf('не принимает приглашения') > -1;
 
                 if (tooOften) {
                     console.log('ТАКИ НАС ОБНАРУЖИЛИ!!');
@@ -116,8 +116,8 @@ function startInviting() {
             });
         }
 
-        var userContainersAmount = $(userContainerSelector).length;
-        var noMoreUsers = userContainerIndex == userContainersAmount;
+        let userContainersAmount = $(userContainerSelector).length;
+        let noMoreUsers = userContainerIndex == userContainersAmount;
 
         if (noMoreUsers) {
             stopInviting();
