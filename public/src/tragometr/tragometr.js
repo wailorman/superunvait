60a6b09a446689367cb902b207dd9cb52fd1cefb
wailorman <wailorman@gmail.com>
2016-02-04 01:26:06 +0300

    refactor(tragometr): Wrap to funcs

--- public/src/tragometr/tragometr.js
+++ public/src/tragometr/tragometr.js
@@ -8,27 +8,39 @@ const ROW__COLUMN = '.charts_list_i_cell';
 const ROW__COLUMN__COUNTER = '.charts_count.__overview';
 
 
-// fix columns width
-$(ROW__untouched).css({minWidth: 105});
+export function calculateTragocent(feedback = 0, reach = 0) {
 
-$(ROW).each((i, row)=> {
+    let tragocent = ( feedback / reach ) * 100;
+    return _.round(tragocent, 2);
 
-    let tragometr = $(row).find('.tragometr');
-    let noTragometrInRowYet = tragometr.length == 0;
+}
 
-    if (noTragometrInRowYet) {
-        // dirty means html from element (may contains &nbsp; and others which
-        // parseInt can't parse correctly)
-        let reachDirty = $($(row).find(ROW__COLUMN)[1]).find(ROW__COLUMN__COUNTER).html();
-        let feedbackDirty = $($(row).find(ROW__COLUMN)[2]).find(ROW__COLUMN__COUNTER).html();
+export function assignTragocentsToPosts(){
 
-        let reach = parseInt(_.join(reachDirty.match(/\d/g), ''));
-        let feedback = parseInt(_.join(feedbackDirty.match(/\d/g), ''));
+    // fix columns width
+    $(ROW__untouched).css({minWidth: 105});
 
-        let tragocent = ( feedback / reach ) * 100;
-        let roundedTragocent = _.round(tragocent, 2);
-        debugger;
-        $(row).append(`<div class="charts_list_i_cell tragometr" style="width:50px;min-width:0px;">${roundedTragocent}</div>`);
-    }
+    $(ROW).each((i, row)=> {
 
-});
+        let tragometr = $(row).find('.tragometr');
+        let noTragometrInRowYet = tragometr.length == 0;
+
+        if (noTragometrInRowYet) {
+            // dirty means html from element (may contains &nbsp; and others which
+            // parseInt can't parse correctly)
+            let reachDirty = $($(row).find(ROW__COLUMN)[1]).find(ROW__COLUMN__COUNTER).html();
+            let feedbackDirty = $($(row).find(ROW__COLUMN)[2]).find(ROW__COLUMN__COUNTER).html();
+
+            let reach = parseInt(_.join(reachDirty.match(/\d/g), ''));
+            let feedback = parseInt(_.join(feedbackDirty.match(/\d/g), ''));
+
+            let tragocent = calculateTragocent( feedback, reach );
+            debugger;
+            $(row).append(`<div class="charts_list_i_cell tragometr" style="width:50px;min-width:0;">${tragocent}</div>`);
+        }
+
+    });
+
+}
+
+export default assignTragocentsToPosts;
