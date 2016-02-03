b9800c2d2ef4479334dd51bbcb87f48e5feb49a1
wailorman <wailorman@gmail.com>
2016-02-03 19:38:18 +0300

    feat(post-hunter): Paint posts on click

--- public/src/content_script.js
+++ public/src/content_script.js
@@ -1,3 +1,11 @@
+import paintPosts from './post-hunter/parsers/ok-ru'
+
+console.log(`content_script`);
+
+chrome.runtime.onMessage.addListener(function(message,sender,sendResponse){
+    console.log(`clicked!`);
+    paintPosts();
+});
 
 $(document).ready(function(){
 	if(document.location.href.indexOf('ok.ru/online')>-1){
