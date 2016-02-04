c9ba956af22270d7595c6e084c8de417c03a5601
wailorman <wailorman@gmail.com>
2016-02-05 02:54:43 +0300

    feat(libs): SPA URL observer

---
+++ public/lib/spa-url-observer.js
@@ -0,0 +1,41 @@
+let interval;
+
+export function onUrlChange(callback) {
+
+    let lastUrl = document.location.href;
+    let currentUrl;
+
+    interval = setInterval(()=> {
+
+        currentUrl = document.location.href;
+        if ( lastUrl != currentUrl ) {
+            callback(currentUrl);
+            lastUrl = currentUrl;
+        }
+
+    }, 500);
+
+    return interval;
+
+}
+
+export function stopListeningUrl() {
+    clearInterval(interval);
+}
+
+export function assignScriptToUrl(urlRegexp) {
+
+    return {
+        onVisit(callback){
+            if (document.location.href.match(urlRegexp)){
+                callback();
+            }
+            onUrlChange((url)=> {
+                if (url.match(urlRegexp)){
+                    callback();
+                }
+            });
+        }
+    };
+
+}
