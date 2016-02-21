fde8fd7b47c7937597c256bd4f8a7eabc9c2b7f3
wailorman <wailorman@gmail.com>
2016-02-21 20:49:29 +0300

    chore(gulp): zip task

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,11 @@
+"use strict";
+
 var gulp = require('gulp');
 var crx = require('gulp-crx-pack');
+var zip = require('gulp-zip');
 var manifest = require('./public/dist/manifest.json');
 var fs = require('fs');
+var pckgInfo = require('./package.json');
 
 gulp.task('crx', function () {
     return gulp.src('./public/dist')
@@ -10,4 +14,10 @@ gulp.task('crx', function () {
             filename: 'ibb-tools.crx'
         }))
         .pipe(gulp.dest('./public'));
+});
+
+gulp.task('zip', function () {
+    return gulp.src('public/dist/*')
+        .pipe(zip(`ibb-tools-${pckgInfo.version}.zip`))
+        .pipe(gulp.dest('public/'));
 });
