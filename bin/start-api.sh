c3495b23fac77e805820a2fd2b3508a82d0e7193
wailorman <wailorman@gmail.com>
2016-02-28 02:42:22 +0300

    fix(bin/start-api.sh): No longer dependence on mysql container

--- bin/start-api.sh
+++ bin/start-api.sh
@@ -6,6 +6,5 @@ docker pull wailorman/ibb-api:latest
 docker rm -f ibb-api-node 2>/dev/null
 docker run -p 8050:8050 \
 --name ibb-api-node \
---link mysql:mysql \
 -e DB_CONNECTION_STRING=mysql://${MYSQL_USER}:${MYSQL_PASSWORD}@${MYSQL_HOST}/${MYSQL_DATABASE} \
 -d wailorman/ibb-api:latest
