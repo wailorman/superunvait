ef2f88956c85a86da0e0687e7ee62040a8be589b
wailorman <wailorman@gmail.com>
2016-02-28 01:28:40 +0300

    feat(bin/sh): Add MYSQL_HOST env variable

--- bin/db-config.sh
+++ bin/db-config.sh
@@ -4,6 +4,7 @@ echo "Specify mysql credentials in bin/db-config.sh!"
 exit # And comment this line
 
 MYSQL_ROOT_PASSWORD=1 #root password
+MYSQL_HOST=mysql
 MYSQL_DATABASE=test #database name
 MYSQL_USER=test #user name
 MYSQL_PASSWORD=123 #user pass
--- bin/start-api.sh
+++ bin/start-api.sh
@@ -7,5 +7,5 @@ docker rm -f ibb-api-node 2>/dev/null
 docker run -p 8050:8050 \
 --name ibb-api-node \
 --link mysql:mysql \
--e DB_CONNECTION_STRING=mysql://${MYSQL_USER}:${MYSQL_PASSWORD}@mysql/${MYSQL_DATABASE} \
+-e DB_CONNECTION_STRING=mysql://${MYSQL_USER}:${MYSQL_PASSWORD}@${MYSQL_HOST}/${MYSQL_DATABASE} \
 -d wailorman/ibb-api:latest
