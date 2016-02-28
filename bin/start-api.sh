64eea32e4777d98724d7511a771890aea4799182
wailorman <wailorman@gmail.com>
2016-02-29 00:31:42 +0300

    feat(bin/start-api): Image tag choosing

--- bin/start-api.sh
+++ bin/start-api.sh
@@ -2,9 +2,11 @@
 
 source ./db-config.sh
 
-docker pull wailorman/ibb-api:latest
+tag=${1:-latest}
+
+docker pull wailorman/ibb-api:${tag}
 docker rm -f ibb-api-node 2>/dev/null
 docker run -p 8050:8050 \
 --name ibb-api-node \
 -e DB_CONNECTION_STRING=${IBB_MYSQL_CONNECTION_STRING} \
--d wailorman/ibb-api:latest
+-d wailorman/ibb-api:${tag}
