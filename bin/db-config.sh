2df5188c7becca1227c4626d697528474af69b32
wailorman <wailorman@gmail.com>
2016-02-29 00:06:05 +0300

    feat(bin/db-config): Try to get IBB_MYSQL_CONNECTION_STRING from env

--- bin/db-config.sh
+++ bin/db-config.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
 
-echo "Specify mysql credentials in bin/db-config.sh!"
-exit # And comment this line
+if [ -z "$IBB_MYSQL_CONNECTION_STRING" ]
+then
+    echo "Specify mysql credentials in bin/db-config.sh!"
+    exit
+fi
+
 
 MYSQL_ROOT_PASSWORD=1 #root password
-MYSQL_HOST=mysql
+MYSQL_HOST=mysql # not for docker mysql
 MYSQL_DATABASE=test #database name
 MYSQL_USER=test #user name
 MYSQL_PASSWORD=123 #user pass
 
-IBB_MYSQL_CONNECTION_STRING=${IBB_MYSQL_CONNECTION_STRING:="mysql://${MYSQL_USER}:${MYSQL_PASSWORD}@${MYSQL_HOST}/${MYSQL_DATABASE}"}
+IBB_MYSQL_CONNECTION_STRING=${IBB_MYSQL_CONNECTION_STRING:?"mysql://${MYSQL_USER}:${MYSQL_PASSWORD}@${MYSQL_HOST}/${MYSQL_DATABASE}"}
