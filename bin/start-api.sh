7d1fd3c2244d65b6e8ddb765970f1f7bc997769e
wailorman <wailorman@gmail.com>
2016-02-28 20:35:18 +0300

    refactor(bin): IBB_MYSQL_CONNECTION_STRING

--- bin/start-api.sh
+++ bin/start-api.sh
@@ -6,5 +6,5 @@ docker pull wailorman/ibb-api:latest
 docker rm -f ibb-api-node 2>/dev/null
 docker run -p 8050:8050 \
 --name ibb-api-node \
--e DB_CONNECTION_STRING=mysql://${MYSQL_USER}:${MYSQL_PASSWORD}@${MYSQL_HOST}/${MYSQL_DATABASE} \
+-e DB_CONNECTION_STRING=${IBB_MYSQL_CONNECTION_STRING} \
 -d wailorman/ibb-api:latest
