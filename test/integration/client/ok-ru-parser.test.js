f5cbd35113a996ccd24907ec9bd187c19f088157
wailorman <wailorman@gmail.com>
2016-03-09 02:17:53 +0300

    test(post hunter / ok.ru parser / paintPosts func): Exception control

--- test/integration/client/ok-ru-parser.test.js
+++ test/integration/client/ok-ru-parser.test.js
@@ -20,17 +20,17 @@ describe("post hunter / parsers / ok-ru", ()=> {
     const likesRanges = [[1, 12], [12, 15], [15, 20], [23, 23], [26, 38], [39, 43], [43, 45], [45, 53],
         [55, 62], [77, 116]];
 
-    describe("getLikesRanges & getArrayOfLikes", ()=> {
+    beforeEach(()=> {
+        //document.body.innerHTML = __html__['test/fixtures/ibb-feed.html'];
+        const fixtureContent = require('raw!../../fixtures/ibb-feed.html');
+        $('#test-zone').html(fixtureContent);
+    });
 
-        beforeEach(()=> {
-            //document.body.innerHTML = __html__['test/fixtures/ibb-feed.html'];
-            const fixtureContent = require('raw!../../fixtures/ibb-feed.html');
-            $('#test-zone').html(fixtureContent);
-        });
+    afterEach(()=> {
+        $('#test-zone').html("");
+    });
 
-        afterEach(()=> {
-            $('#test-zone').html("");
-        });
+    describe("getLikesRanges", ()=> {
 
         it(`should return expected ranges`, () => {
 
@@ -41,12 +41,28 @@ describe("post hunter / parsers / ok-ru", ()=> {
 
         });
 
+    });
+
+    describe("getArrayOfLikes", ()=> {
+
         it(`should return expected array of likes`, () => {
 
             const expected = arrayOfLikes;
             const actual = getArrayOfLikes();
 
             expect(actual).to.eql(expected);
+
+        });
+
+    });
+
+    describe("paintPosts", ()=> {
+
+        it(`should not throw any error`, () => {
+
+            expect(()=> {
+                paintPosts();
+            }).to.not.throw();
 
         });
 
