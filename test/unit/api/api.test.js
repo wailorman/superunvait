4578d66a4bab7799b14f48fb152323b1868403c2
wailorman <wailorman@gmail.com>
2016-03-08 03:54:22 +0300

    fix(webpack mocha testing): Using sinon

--- test/unit/api/api.test.js
+++ test/unit/api/api.test.js
@@ -5,6 +5,7 @@ describe("desc!", ()=> {
     it(`123__4`, () => {
         expect(true).to.eql(true);
         console.log(`1234`);
+        if (sinon) console.log(`I've found sinon!`);
     });
 
 });
