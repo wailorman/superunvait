52f643d5846f54dcaa95f8f15139ae80fdcc0c61
wailorman <wailorman@gmail.com>
2016-03-15 01:01:23 +0300

    feat(api/ok): OK API facade w/ promises

---
+++ src/modules/ok-api-facade.js
@@ -0,0 +1,40 @@
+"use strict";
+
+const ok = require('ok.ru');
+const Q = require('q');
+
+const okCredentials = process.env.OK_CREDENTIALS;
+const splitedCredentials = okCredentials.split(';');
+
+const requestOptions = {
+    applicationId: splitedCredentials[0],
+    applicationKey: splitedCredentials[1],
+    applicationSecretKey: splitedCredentials[2]
+};
+
+ok.setOptions(requestOptions);
+ok.setAccessToken(splitedCredentials[3]);
+
+const universalFunction = function (method, requestParameters) {
+    let nodefiedRequest;
+
+    if (method.toUpperCase() == 'GET') {
+        nodefiedRequest = Q.denodeify(ok.get);
+    }else if (method.toUpperCase() == 'POST') {
+        nodefiedRequest = Q.denodeify(ok.post);
+    }else{
+        throw 'Accepting only POST or GET methods';
+    }
+
+    return nodefiedRequest(requestParameters)
+        .then((data)=> {
+            return data[0];
+        });
+};
+
+const okFacade = {
+    get: universalFunction.bind(this, 'GET'),
+    post: universalFunction.bind(this, 'POST')
+};
+
+module.exports = okFacade;
