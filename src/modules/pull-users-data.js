33eb2f6334368b8ddeb5f45bb536c2d94c532f1c
wailorman <wailorman@gmail.com>
2016-03-15 02:29:09 +0300

    feat(api/members observer): pullUsersInfo

---
+++ src/modules/pull-users-data.js
@@ -0,0 +1,103 @@
+"use strict";
+
+const okApi = require('./ok-api-facade');
+const models = require('../../models/');
+const User = models.user;
+const _ = require('lodash');
+const sequelize = require('../../models/index').sequelize;
+const Q = require('q');
+
+const fieldsRequiredForUsersGetInfo = [
+    'uid',
+    'name',
+    'age',
+    'allows_anonym_access',
+    'allows_messaging_only_for_friends',
+    'birthday',
+    'gender',
+    'last_online',
+    'location',
+    'photo_id',
+    'has_service_invisible',
+    'private'
+];
+const joinedFields = fieldsRequiredForUsersGetInfo.join(',');
+
+const adoptPulledDataToDbFormat = function (dataFromApi) {
+
+    let dataForDB = {};
+
+    dataForDB.allowsAnonymAccess = dataFromApi.allows_anonym_access;
+    dataForDB.allowsMessagingOnlyForFriends = dataFromApi.allows_messaging_only_for_friends;
+    dataForDB.lastOnline = dataFromApi.last_online;
+    dataForDB.photoId = dataFromApi.photo_id;
+    dataForDB.hasServiceInvisible = dataFromApi.has_service_invisible;
+
+    switch (dataFromApi.gender) {
+        case 'male':
+            dataForDB.gender = 'M';
+            break;
+        case 'female':
+            dataForDB.gender = 'F';
+            break;
+    }
+
+    if (dataFromApi.location) {
+        dataForDB.city = dataFromApi.location.city;
+        dataForDB.countryName = dataFromApi.location.countryName;
+    }
+
+    const fieldsToPickFromApiResponse = _.without(
+        fieldsRequiredForUsersGetInfo, 'gender', 'location',
+        'allows_anonym_access', 'allows_messaging_only_for_friends', 'last_online', 'photo_id', 'has_service_invisible'
+    );
+
+    const cuttedDataFromApi = _.pick(dataFromApi, fieldsToPickFromApiResponse);
+
+    return _.merge(cuttedDataFromApi, dataForDB);
+
+};
+
+const pullUsersInfo = function (userIds) {
+
+    let requestParameters = {
+        method: 'users.getInfo',
+        uids: userIds.join(',') || userIds,
+        fields: joinedFields
+    };
+
+    okApi.get(requestParameters)
+        .then((data)=> {
+
+            sequelize.transaction((t1)=> {
+
+                let transactionQueries = [];
+
+                data.forEach((user)=> {
+
+                    const adoptedUser = adoptPulledDataToDbFormat(user);
+
+                    transactionQueries.push(
+                        User.upsert(
+                            adoptedUser,
+                            {
+                                transaction: t1,
+                                validate: true
+                            }
+                        )
+                    );
+
+                });
+
+                return Q.all([transactionQueries]);
+
+            });
+        })
+        .catch((err)=> {
+            debugger;
+            return err;
+        });
+
+};
+
+module.exports = {pullUsersInfo};
---
+++ src/test-ok-ru.js
@@ -0,0 +1,34 @@
+const pullUsersData = require('./modules/pull-users-data');
+
+pullUsersData.pullUsersInfo(['571769013138, 558123591415']);
+
+/*
+uid
+name
+age
+allows_anonym_access
+allows_messaging_only_for_friends
+birthday
+gender
+last_online
+location.city = city
+location.countryName = country
+photo_id
+has_service_invisible
+private
+
+uid
+name
+age
+allows_anonym_access
+allows_messaging_only_for_friends
+birthday
+gender
+last_online
+location.city
+location.countryName
+photo_id
+has_service_invisible
+private
+
+*/
