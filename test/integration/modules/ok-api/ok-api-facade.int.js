37d853bc03c20df4986e56042c76d058faa2f338
wailorman <wailorman@gmail.com>
2016-03-21 01:39:10 +0300

    test(ok-api facade): Pass requests to OK api through nock

--- test/integration/modules/ok-api/ok-api-facade.int.js
+++ test/integration/modules/ok-api/ok-api-facade.int.js
@@ -1,10 +1,33 @@
 "use strict";
 
+const nock = require('nock');
+
 const okApi = require('../../../../src/modules/ok-api/ok-api-facade');
+const okApiHelpers = require('../../../../src/modules/ok-api/helpers');
+const signatureCalculator = require('../../../../src/modules/ok-api/signature-calculator');
+const fixtures = require('../../../fixtures/users.getInfo.json');
+
+const credentials = okApiHelpers.getCredentialsByStr(process.env.OK_CREDENTIALS);
 
 describe("ok api facade", ()=> {
 
     it(`should pull some user info`, () => {
+
+        let query = {
+            method: "users.getInfo",
+            fields: "*",
+            uids: "571769013138",
+
+            application_key: credentials.applicationKey
+        };
+
+        query.sig = signatureCalculator.calculateSignature(query,  credentials);
+        query.access_token = credentials.accessToken;
+
+        const usersGetInfoMock = nock('http://api.odnoklassniki.ru')
+            .get('/fb.do')
+            .query(query)
+            .reply(200, [fixtures[1]]);
 
         let requestParameters = {
             method: 'users.getInfo',
@@ -15,6 +38,10 @@ describe("ok api facade", ()=> {
         return okApi.get(requestParameters)
             .then((data)=> {
                 expect(data[0].uid).to.eql('571769013138');
+                return data;
+            })
+            .finally(()=> {
+                expect(usersGetInfoMock.isDone()).to.eql(true);
             });
 
     });
