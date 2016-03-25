bfaf59c05cab6f1acc1aa255729b4e3b72acb518
wailorman <wailorman@gmail.com>
2016-03-25 22:52:13 +0300

    test(ok-api / get group members): Describe test cases

--- test/integration/modules/ok-api/get-group-members.int.js
+++ test/integration/modules/ok-api/get-group-members.int.js
@@ -1,16 +1,20 @@
-/*
+"use strict";
+
+describe("OK API / get group members", ()=> {
 
 
-getLastMembersUids
-    should not call doMembersGetRequest twice if response is []
-    should call doMembersGetRequest twice if [...],[]
+    describe("getLastMembersUids", ()=> {
+
+        //should write 1 member
+        //should write 100 members
+        //should write 101 members with 2 requests
+        //should write 201 members with three requests
+        //should not write anything if members amount = 0
+        //
+        //should request twice if first response return only half
+        //should calm down on second request if requested 100, but there are only 50
+
+    });
 
 
-
-doMembersGetRequest
-
-
-
-
-
-*/
+});
--- test/unit/modules/ok-api/get-group-members.unit.js
+++ test/unit/modules/ok-api/get-group-members.unit.js
@@ -1,15 +1,51 @@
-/*
+"use strict";
 
-parseResponse
-    should convert members to flat array
-    should append anchor to result
+describe("OK API / get group members", ()=> {
 
-    no members field -> throw
-    members array if empty -> members:[]
-    anchor didn't resolved -> without anchor
-    in some of objects disappeared userId field
+    describe("parseResponse", ()=> {
+
+        // should convert members to flat array
+        // should append anchor to result
+
+        // no members field -> throw MEMBERS_MISSING
+        // members array if empty -> members:[]
+        // anchor didn't resolved -> throw ANCHOR_MISSING
+        // in some of objects disappeared userId field
+
+        // should remove members duplicates
+
+    });
+
+    describe("verifyMembers", ()=> {
+
+        // SUCCESS if all goes normal
+        // NO_MEMBERS_ANY_MORE if members array is empty
+        // throw INVALID_USER_OBJECT if some of users aren't correct
+        // throw ARRAY_MISSING if members array isn't defined
+
+    });
+
+    describe("verifyAnchor", ()=> {
+
+        // SUCCESS if anchor="abc123"
+        // throw ANCHOR_MISSING if anchor=""
+        // throw ANCHOR_MISSING if anchor=null
+
+    });
+
+    describe("validateResponse", ()=> {
+
+        // should return 1 if all OK
+        // should return 0 if response not valid
+
+    });
+
+    describe("doMembersGetRequest", ()=> {
+
+        // should make correct request
+        // should reject error if response is invalid
+
+    });
 
 
-
-
- */
+});
