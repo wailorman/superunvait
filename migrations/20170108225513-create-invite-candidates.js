bc7b8cfadcbf66e6f9349e998e8f4f3a101e3458
wailorman <wailorman@yandex.ru>
2017-01-09 02:44:43 +0300

    fix(server/models/invite-candidate): Rename model #2

--- migrations/20170108225513-create-invite-candidates.js
+++ migrations/20170108225513-create-invite-candidates.js
@@ -2,14 +2,10 @@
 module.exports = {
   up: function(queryInterface, Sequelize) {
     return queryInterface.createTable('invite-candidates', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
       userId: {
-        type: Sequelize.STRING
+          type: Sequelize.STRING,
+          allowNull: false,
+          primaryKey: true
       },
       createdAt: {
         allowNull: false,
