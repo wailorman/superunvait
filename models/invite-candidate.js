3ceec7ac8594ea676b22c828e6d0ad05060b4c87
wailorman <wailorman@yandex.ru>
2017-01-09 02:47:54 +0300

    fix(server/models/invite-candidate): Rename model #3

--- models/invite-candidate.js
+++ models/invite-candidate.js
@@ -1,13 +1,16 @@
 'use strict';
 module.exports = function(sequelize, DataTypes) {
-  var InviteCandidate = sequelize.define('invite-candidate', {
-    userId: DataTypes.STRING
-  }, {
-    classMethods: {
-      associate: function(models) {
-        // associations can be defined here
-      }
-    }
-  });
-  return InviteCandidate;
+    const InviteCandidate = sequelize.define('invite-candidate', {
+        userId: {
+            type: DataTypes.STRING,
+            primaryKey: true
+        }
+    }, {
+        classMethods: {
+            associate: function(models) {
+                // associations can be defined here
+            }
+        }
+    });
+    return InviteCandidate;
 };
