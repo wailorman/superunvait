5cedbbcbd0841804e9b1d2d8dc1d3d6225ac2232
wailorman <wailorman@yandex.ru>
2017-01-10 00:05:42 +0300

    feat(server/observer/get-unfilled): Add querying unfilled users

---
+++ src/modules/get-unfilled-uids.js
@@ -0,0 +1,26 @@
+const fs = require('fs-promise');
+const sequelize = require('../../models/index').sequelize;
+
+// from all resources
+const fromAll = () => {
+
+    return fs.readFile(__dirname + '/../../queries/unfilled-users.sql', 'utf8')
+        .then((sqlQuery) => {
+
+            return sequelize.query(sqlQuery, { type: sequelize.QueryTypes.SELECT });
+
+        })
+        .then((res) => {
+
+            return res.map(res => res.id);
+
+        });
+
+};
+
+
+fromAll();
+
+module.exports = {
+    fromAll
+};
