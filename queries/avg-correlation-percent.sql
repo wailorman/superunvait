ae1640c6b3d510e35534bc9e8859df76761d760d
wailorman <wailorman@yandex.ru>
2017-01-10 18:11:32 +0300

    feat(queries/avg correlation percent): Create

---
+++ queries/avg-correlation-percent.sql
@@ -0,0 +1,48 @@
+SELECT
+    splitted_invites_to_grps.groupsS,
+    -- (COUNT(*) / _total.totalCnt - 1 + splitted_invites_to_grps.joined ) * 100 AS "%",
+    _accepted.cnt / _total.cnt * 100 AS "%",
+    _accepted.cnt AS "Приняли",
+    _declined.cnt AS "Отказали",
+    _total.cnt AS "Всего"
+FROM
+    splitted_invites_to_grps
+
+
+LEFT JOIN (
+        SELECT
+            groupsS,
+            COUNT(*) AS cnt
+        FROM
+            splitted_invites_to_grps
+         WHERE joined = '1'
+        GROUP BY groupsS
+) AS _accepted ON _accepted.groupsS = splitted_invites_to_grps.groupsS
+
+
+LEFT JOIN (
+        SELECT
+            groupsS,
+            COUNT(*) AS cnt
+        FROM
+            splitted_invites_to_grps
+         WHERE joined = '0'
+        GROUP BY groupsS
+) AS _declined ON _declined.groupsS = splitted_invites_to_grps.groupsS
+
+
+LEFT JOIN (
+        SELECT
+            groupsS,
+            COUNT(*) AS cnt
+        FROM
+            splitted_invites_to_grps
+        GROUP BY groupsS
+) AS _total ON _total.groupsS = splitted_invites_to_grps.groupsS
+
+
+GROUP BY splitted_invites_to_grps.groupsS
+
+ORDER BY
+    "%" DESC,
+    splitted_invites_to_grps.groupsS ASC;
