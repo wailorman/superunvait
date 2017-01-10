67685627c34f5f6eabed108cf96fdad20c599f5d
wailorman <wailorman@yandex.ru>
2017-01-10 04:42:13 +0300

    feat(queries/avg correlation): Add registeredDate correlation

--- queries/avg-correlation.sql
+++ queries/avg-correlation.sql
@@ -1,12 +1,13 @@
 (
 SELECT
-"joined" AS _,
+"joined"        AS _,
 AVG(age)        as avg_age,
 AVG(groups)     as avg_groups,
 AVG(games)      as avg_games,
 AVG(friends)    as avg_friends,
 AVG(notes)      as avg_notes,
-AVG(photos)     as avg_photos
+AVG(photos)     as avg_photos,
+AVG( DATEDIFF( NOW(), registeredDate ) ) as avg_daysBetwReg
 FROM
 extended_invites
 WHERE joinedAt IS NOT NULL
@@ -14,13 +15,14 @@ WHERE joinedAt IS NOT NULL
 UNION ALL
 (
 SELECT
-"not joined" _,
+"not joined"    AS _,
 AVG(age)        as avg_age,
 AVG(groups)     as avg_groups,
 AVG(games)      as avg_games,
 AVG(friends)    as avg_friends,
 AVG(notes)      as avg_notes,
-AVG(photos)     as avg_photos
+AVG(photos)     as avg_photos,
+AVG( DATEDIFF( NOW(), registeredDate ) ) as avg_daysBetwReg
 FROM
 extended_invites
 WHERE joinedAt IS NULL
@@ -37,25 +39,27 @@ SELECT
 
 "diff" AS _,
 
-joined.avg_age        - notJoined.avg_age       AS dif_age,
-joined.avg_groups     - notJoined.avg_groups    AS dif_groups,
-joined.avg_games      - notJoined.avg_games     AS dif_games,
-joined.avg_friends    - notJoined.avg_friends   AS dif_friends,
-joined.avg_notes      - notJoined.avg_notes     AS dif_notes,
-joined.avg_photos     - notJoined.avg_photos    AS dif_photos
+joined.avg_age          - notJoined.avg_age             AS dif_age,
+joined.avg_groups       - notJoined.avg_groups          AS dif_groups,
+joined.avg_games        - notJoined.avg_games           AS dif_games,
+joined.avg_friends      - notJoined.avg_friends         AS dif_friends,
+joined.avg_notes        - notJoined.avg_notes           AS dif_notes,
+joined.avg_photos       - notJoined.avg_photos          AS dif_photos,
+joined.avg_daysBetwReg  - notJoined.avg_daysBetwReg     AS dif_daysBetwReg
 
 FROM
 
 
 (
 SELECT
-joinedAt IS NOT NULL AS joined,
+"joined"        AS _,
 AVG(age)        as avg_age,
 AVG(groups)     as avg_groups,
 AVG(games)      as avg_games,
 AVG(friends)    as avg_friends,
 AVG(notes)      as avg_notes,
-AVG(photos)     as avg_photos
+AVG(photos)     as avg_photos,
+AVG( DATEDIFF( NOW(), registeredDate ) ) as avg_daysBetwReg
 FROM
 extended_invites
 WHERE joinedAt IS NOT NULL
@@ -64,13 +68,14 @@ WHERE joinedAt IS NOT NULL
 
 (
 SELECT
-joinedAt IS NOT NULL AS joined,
+"not joined"    AS _,
 AVG(age)        as avg_age,
 AVG(groups)     as avg_groups,
 AVG(games)      as avg_games,
 AVG(friends)    as avg_friends,
 AVG(notes)      as avg_notes,
-AVG(photos)     as avg_photos
+AVG(photos)     as avg_photos,
+AVG( DATEDIFF( NOW(), registeredDate ) ) as avg_daysBetwReg
 FROM
 extended_invites
 WHERE joinedAt IS NULL
