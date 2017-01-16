b4d0fcde851dcb9c16eb46bd0321f40c7143dfe3
wailorman <wailorman@yandex.ru>
2017-01-16 20:57:01 +0300

    feat(mysql/queires/conversion by day): Add avg_* columns

--- queries/conversion-by-day.sql
+++ queries/conversion-by-day.sql
@@ -2,11 +2,32 @@ SELECT
     DATE(invites.createdAt) AS day,
     TRUNCATE( SUM(members.id IS NOT NULL) / COUNT(*) * 100, 2) AS conversion,
     COUNT(*) AS invites,
-    SUM(members.id IS NOT NULL) AS joined
+    SUM(members.id IS NOT NULL) AS joined,
+
+
+
+    TRUNCATE( AVG(groups) , 2)             as avg_groups,
+    TRUNCATE( AVG(notes) , 2)              as avg_notes,
+    TRUNCATE( AVG(friends) , 2)            as avg_friends,
+    TRUNCATE( AVG(photos) , 2)             as avg_photos,
+    TRUNCATE( AVG(age) , 2)                as avg_age,
+    TRUNCATE( AVG(games) , 2)              as avg_games,
+
+    TRUNCATE(
+        AVG(
+            DATEDIFF(
+                NOW(),
+                registeredDate
+            )
+        )
+    , 2)                                    as avg_daysBetwReg
+
+
 FROM
     invites
 
 LEFT JOIN members ON invites.userId = members.id
+LEFT JOIN users ON invites.userId = users.uid
 
 GROUP BY day
 
