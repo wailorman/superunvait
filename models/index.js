e6c8b8d934251cf527460e343b7936a253f8f96b
wailorman <wailorman@yandex.ru>
2017-01-17 19:31:14 +0300

    fix(server/sequelize config): Passing host from config to constructor

--- models/index.js
+++ models/index.js
@@ -12,7 +12,10 @@ var sequelize;
 if (config.use_env_variable) {
     sequelize = new Sequelize(process.env[config.use_env_variable], {logging: false});
 } else {
-    sequelize = new Sequelize(config.database, config.username, config.password, {logging: false});
+    sequelize = new Sequelize(config.database, config.username, config.password, {
+        host: config.host,
+        logging: false
+    });
 }
 
 fs.readdirSync(__dirname)
