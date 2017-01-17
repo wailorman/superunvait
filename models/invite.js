8f1f0e95a104027ba35a142b27c34d0500800b69
wailorman <wailorman@yandex.ru>
2017-01-17 19:34:16 +0300

    feat(server/invites): Use retry library for findAll & create

--- models/invite.js
+++ models/invite.js
@@ -1,14 +1,27 @@
 'use strict';
+
+const retry = require('../src/lib/sequelize-retry');
+
 module.exports = function(sequelize, DataTypes) {
-  return sequelize.define('invite', {
-    userId: DataTypes.STRING,
-    city: DataTypes.STRING,
-    senderId: DataTypes.STRING
-  }, {
-    classMethods: {
-      associate: function (models) {
-        // associations can be defined here
-      }
-    }
-  });
+    return sequelize.define('invite', {
+        userId: DataTypes.STRING,
+        city: DataTypes.STRING,
+        senderId: DataTypes.STRING
+    }, {
+        classMethods: {
+            associate: function(models) {
+                // associations can be defined here
+            },
+            findAll: function(...args) {
+                return retry(
+                    this.constructor.prototype.findAll.bind(this)
+                )(...args);
+            },
+            create: function(...args) {
+                return retry(
+                    this.constructor.prototype.create.bind(this)
+                )(...args);
+            }
+        }
+    });
 };
