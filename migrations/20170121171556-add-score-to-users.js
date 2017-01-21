b375877063312d35df5e3e502201aebbdb17c49e
wailorman <wailorman@yandex.ru>
2017-01-22 00:33:19 +0300

    fix(mysql/users/#score): Set FLOAT w/out length limits

--- migrations/20170121171556-add-score-to-users.js
+++ migrations/20170121171556-add-score-to-users.js
@@ -1,34 +1,30 @@
 'use strict';
 
 module.exports = {
-  up: function (queryInterface, Sequelize) {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
+    up: function(queryInterface, Sequelize) {
+        /*
+         Add altering commands here.
+         Return a promise to correctly handle asynchronicity.
 
-      Example:
-      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
-    */
+         Example:
+         return queryInterface.createTable('users', { id: Sequelize.INTEGER });
+         */
 
-      return [
-          queryInterface.addColumn('users', 'score', {
-              type: Sequelize.FLOAT(2),
-              defaultValue: 0,
-              allowNull: true
-          })
-      ];
-  },
+        return queryInterface.addColumn('users', 'score', {
+            type: Sequelize.FLOAT,
+            defaultValue: 0,
+            allowNull: true
+        });
+    },
 
-  down: function (queryInterface, Sequelize) {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
+    down: function(queryInterface, Sequelize) {
+        /*
+         Add reverting commands here.
+         Return a promise to correctly handle asynchronicity.
 
-      Example:
-      return queryInterface.dropTable('users');
-    */
-      return [
-          queryInterface.removeColumn('users', 'score')
-      ];
-  }
+         Example:
+         return queryInterface.dropTable('users');
+         */
+        return queryInterface.removeColumn('users', 'score');
+    }
 };
