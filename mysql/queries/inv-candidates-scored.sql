6bf477fa8dcd1d99f936e490a0b5e1ecbd103e1b
wailorman <wailorman@yandex.ru>
2017-01-21 19:58:52 +0300

    feat(mysql/views/inv_candidates_scored): Better score calculation

--- mysql/queries/inv-candidates-scored.sql
+++ mysql/queries/inv-candidates-scored.sql
@@ -1,7 +1,12 @@
 SELECT
 	*,
-	(groups /100 * 10) +
-	(friends /100 * 1) AS score
+	IBB_SCORE(
+	    inv_candidates.friends,
+        inv_candidates.photos,
+        inv_candidates.groups,
+        inv_candidates.notes,
+        inv_candidates.games
+	) AS score
 FROM
 	inv_candidates
 ORDER BY score DESC;
