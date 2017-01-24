7c41f3ed3ab897424e6ebf8014fdbd5ac391ceb2
wailorman <wailorman@yandex.ru>
2017-01-24 15:38:15 +0300

    refactor(libs/retry): Use Array.some()

--- src/lib/sequelize-retry.js
+++ src/lib/sequelize-retry.js
@@ -5,11 +5,16 @@ const msecRetryDefault = 25000;
 const keepRetry = (func, msecRetry = msecRetryDefault) => (...args) => {
 
     const isConnectionError = (err) => {
-        return err instanceof sequelize.ConnectionError
-            || err.original.code == 'ER_LOCK_DEADLOCK'
-            || err instanceof sequelize.ConnectionRefusedError
-            || err instanceof sequelize.HostNotFoundError
-            || err instanceof sequelize.ConnectionTimedOutError;
+
+        const errorsToRetry = [
+            err instanceof sequelize.ConnectionError,
+            err.original.code == 'ER_LOCK_DEADLOCK',
+            err instanceof sequelize.ConnectionRefusedError,
+            err instanceof sequelize.HostNotFoundError,
+            err instanceof sequelize.ConnectionTimedOutError
+        ];
+
+        return errorsToRetry.some( (e) => e );
     };
 
     let failTimestamp = new Date().getTime();
