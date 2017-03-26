9f0441157960d7448cb55ac5904367a65c522058
wailorman <wailorman@yandex.ru>
2017-03-26 11:31:41 +0300

    Fix backup-mysql.sh

--- bin/backup-mysql.sh
+++ bin/backup-mysql.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-CUR_DATE = "$(date +"%Y-%m-%d_%H-%M")"
-MYSQL_DATA_DIR = "/root/db/data"
-BACKUP_DIR = "/root/backups/$DATE"
+CUR_DATE="$(date +"%Y-%m-%d_%H-%M")"
+MYSQL_DATA_DIR="/root/db/data"
+BACKUP_DIR="/root/backups/$CUR_DATE"
 
 xtrabackup --backup --datadir="$MYSQL_DATA_DIR" --target-dir="$BACKUP_DIR" --host=127.0.0.1 --port=3306 --user=root
 xtrabackup --prepare --target-dir="$BACKUP_DIR"
